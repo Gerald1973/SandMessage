bd59d2b8448936db8bc39b5d11d63e1afb8a1f29
marechal <gerald1973@gmail.com>
2016-02-02 15:06:43 +0100

    ...

--- SandMessage/src/main/java/com/smilesmile1973/sandmessage/controller/WelcomeController.java
+++ SandMessage/src/main/java/com/smilesmile1973/sandmessage/controller/WelcomeController.java
@@ -2,16 +2,13 @@ package com.smilesmile1973.sandmessage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/index")
 public class WelcomeController {
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping("/welcome")
 	public ModelAndView welcome() {
 		ModelAndView model = new ModelAndView("index");
 		return model;
 	}
-
 }
